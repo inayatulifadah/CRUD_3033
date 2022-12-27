d2425c604cf47805ea1c16b643870ade25dc415e
inayatulifadah <115690483+inayatulifadah@users.noreply.github.com>
2022-12-27 11:36:43 +0700

    Add files via upload

---
+++ CrudJava_3033/src/main/java/com/inayatulifadah/crudmahasiswa/CrudMahasiswa.java
@@ -0,0 +1,20 @@
+package com.inayatulifadah.crudmahasiswa;
+
+
+import com.zidanfathannul.crudmahasiswasederhana.views.MahasiswaFrame;
+import javax.swing.SwingUtilities;
+
+public class CrudMahasiswa {
+    
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+           
+            public void run() {
+                MahasiswaFrame form = new MahasiswaFrame();
+                form.setLocationRelativeTo(null);
+                form.setVisible(true);
+                
+            }
+        });
+    }
+}
